51cd3b5b5feffd598fa0ec458bdc01fbb3bbfdf5
geoffroy.cochard <geoffroy.cochard@orleans-metropole.fr>
2024-09-02 16:08:02 +0200

    Layout email

--- deploy.php
+++ deploy.php
@@ -8,6 +8,7 @@ set('repository', 'git@github.com:geoffroycochard/opebo.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true);
+set('http_user', 'www-data_ADMIN-OPE');
 
 // Hosts
 host('staging')
@@ -29,10 +30,10 @@ host('prod')
 after('deploy:failed', 'deploy:unlock');
 
 task('restart:php-fpm', function () {
-    run('sudo systemctl restart php8.2-fpm.service');
+    run('sudo systemctl reload php8.2-fpm.service');
     // run('reloadphp82');
 });
-// after('deploy:symlink', 'restart:php-fpm');
+after('deploy:symlink', 'restart:php-fpm');
 
 task('test', function () {
     run('cd /var/www/html/ORLEANS/ORLEANS-METROPOLE/dev.orleans-metropole.fr/Root-TYPO3/current 2>/dev/null || cd /var/www/html/ORLEANS/ORLEANS-METROPOLE/dev.orleans-metropole.fr/Root-TYPO3;echo "yo";');
