810288f4b42aa71d7f49b41e3b0eb1bd206f2036
geoffroy.cochard <geoffroy.cochard@orleans-metropole.fr>
2025-04-03 21:42:40 +0200

    Correction de l'adresse IP dans la configuration SFTP, suppression du lien de connexion dans l'email de confirmation d'inscription et mise à jour du template d'email pour un meilleur formatage.

--- src/EventListener/PersonLeadCreationNotifier.php
+++ src/EventListener/PersonLeadCreationNotifier.php
@@ -4,41 +4,30 @@ declare(strict_types=1);
 namespace App\EventListener;
 
 use App\Entity\Person;
-use App\Repository\PersonRepository;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Event\PostPersistEventArgs;
 use Doctrine\ORM\Events;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Security\Http\LoginLink\LoginLinkHandlerInterface;
 
 #[AsEntityListener(event: Events::postPersist, method: 'postPersist', entity: Person::class)]
 class PersonLeadCreationNotifier
 {
     public function __construct(
         private readonly MailerInterface $mailer,
-        private readonly PersonRepository $personRepository,
-        private readonly LoginLinkHandlerInterface $loginLinkHandler,
         #[Autowire('%admin_email%')] private string $adminEmail,
     ) {}
 
     public function postPersist(Person $person, PostPersistEventArgs $event): void
     {
-
-        // Send poposal to sponsor
-        $user = $this->personRepository->findOneBy(['email' => $person->getEmail()]);
-        $loginLinkDetails = $this->loginLinkHandler->createLoginLink($user);
-        $loginLink = $loginLinkDetails->getUrl();
-
         $email = (new TemplatedEmail())
             ->from($this->adminEmail)
             ->to(new \Symfony\Component\Mime\Address($person->getEmail()))
             ->subject('Confirmation de votre inscription')
             ->htmlTemplate('emails/registration.html.twig')
             ->context([
-                'person' => $person,
-                'login_link' => $loginLink
+                'person' => $person
             ])
         ;
         $this->mailer->send($email);
